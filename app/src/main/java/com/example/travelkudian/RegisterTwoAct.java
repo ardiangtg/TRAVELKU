b8161aceb85d20daded409e9400e7b620c47ec4e
ardiangtg <1711500018@mahasiswa.atmaluhur.ac.id>
2020-12-01 22:58:38 +0700

    terbaru

---
+++ app/src/main/java/com/example/travelkudian/RegisterTwoAct.java
@@ -0,0 +1,14 @@
+package com.example.travelkudian;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterTwoAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_two);
+    }
+}
